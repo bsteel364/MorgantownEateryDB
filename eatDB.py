7feb6c174c93f444a4ea255e6f4b0daed2d885a4
Brandon Steel <bsteel364@gmail.com>
2019-11-21 17:32:45 -0500

    Python Script WIth GUI

---
+++ eatDB.py
@@ -0,0 +1,66 @@
+#Author - Brandon Steel
+
+#import nltk
+#nltk.download()
+#nltk.download('punkt')
+import tkinter as tk
+from tkinter import *
+from nltk import sent_tokenize
+import re
+
+#Create a list of words from the input file
+file = open("data.txt", "r")
+
+
+rows = sent_tokenize(file.read())
+
+input = ""
+
+#GUI Stuff
+def show_entry_fields():
+    outfile = open("output_file.txt", "w")
+    root = Tk()
+    input = e1.get()
+
+    i = 0
+    datasize = len(rows)
+    returned_rows = []
+    while (i < datasize - 1):
+        if (input.lower() in rows[i].lower()):
+            returned_rows.append(rows[i])
+            outfile.write(rows[i])
+            outfile.write('\n')
+        i = i + 1
+    e1.delete(0, END)
+
+    scrollbar = Scrollbar(root)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    mylist = Listbox(root, yscrollcommand=scrollbar.set)
+    for row_count in range(len(returned_rows)):
+        T = Text(root, height=2, width=100)
+        T.pack()
+        T.insert(END, returned_rows[row_count])
+        mylist.insert(END, T)
+    mylist.pack(side=LEFT, fill=BOTH)
+    root.mainloop()
+    outfile.close()
+
+
+master = tk.Tk()
+tk.Label(master,text="Search Term").grid(row=0)
+e1 = tk.Entry(master)
+
+e1.grid(row=0, column=1)
+tk.Button(master,
+          text='Search', command=show_entry_fields).grid(row=3,
+                                                       column=1,
+                                                       sticky=tk.W,
+                                                       pady=4)
+
+
+tk.mainloop()
+
+
+
+
+
